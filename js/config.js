2b87610bc06be1f204b7e9ce113ab2a7f8017ef3
Lisprocoin <83799681+15-Lippo@users.noreply.github.com>
2023-02-14 17:58:10 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -7,13 +7,13 @@
 var FeeReciever = "0xD0355200111C2B21AAbC1a31552eCCDc5d4E905d";
 
 
-var BinanceFee=1;
-var PolygonFee=10;
+var BinanceFee=0.2;
+var PolygonFee=20;
 var FantomFee=10;
-var RopstenFee=0.001;
+var RopstenFee=0.01;
 var HuobiFee=10;
 var AvalancheFee=10;
-var EthereumFee=0.1;
+var EthereumFee=0.01;
 var CronosFee=30;
 var PhoenixFee=10;
 var CandleFee=10;
