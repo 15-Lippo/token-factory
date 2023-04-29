e906cb42b2a33427ab31eb36b6cb1b6c623910fc
Lisprocoin <83799681+15-Lippo@users.noreply.github.com>
2023-04-29 12:18:35 +0200

    Update scan.js

--- js/scan.js
+++ js/scan.js
@@ -3,8 +3,8 @@ var ScanInfo =  {
 	"3": "https://ropsten.etherscan.io",
 	"4": "https://rinkeby.etherscan.io", 
 	"5": "https://goerli.etherscan.io", 
-
-
+        "80001":"https://rpc-mumbai.maticvigil.com/"
+        "137" : "https://rpc-mainnet.maticvigil.com/"
 
 
 
